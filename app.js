424b965feb953483ac40b3cf3047cac5fd1b95cf
samaypiduru <67844360+samaypiduru@users.noreply.github.com>
2020-12-23 18:31:01 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ TO-DO LIST:
 */
 
 var board;
+var userTurn = true;
 const boardElements = [
     "box0", "box1", "box2",
     "box3", "box4", "box5",
@@ -24,19 +25,20 @@ const wins = [
 ];
 
 resetBoard();
-//checkGameOver();
 
 // resets board at the start of the game
 function resetBoard() {
-    board = ["", "", "", "", "", "", "", "", ""];
+    board = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ];
     document.getElementById("gameInfo").innerHTML = "Begin";
-    document.getElementById("gameInfo").value = "user turn";
+    userTurn = true;
     showBoard();
     for(let i = 0; i < boardElements.length; i++) {
         document.getElementById(boardElements[i]).style.removeProperty("background-color");
-
     }
-    //document.getElementsByClassName("space").style.backgroundColor = "";
 }
 
 // assigns each div its board value by iterating through the div names
@@ -47,32 +49,31 @@ function showBoard() {
 }
 
 // checks if the board space is open and takes in user move, then the computer makes the move
-function playerMove(spaceIndex) {
-    if(board[spaceIndex] == "" && !checkGameOver() && document.getElementById("gameInfo").value != "cpu turn") {
+function handleMove(spaceIndex) {
+    if(isOpenSpace(spaceIndex) && !checkGameOver() && userTurn) {
         board[spaceIndex] = "X";
         showBoard();
+        userTurn = false;
         
         if(!checkGameOver()) {
-            computerMove();
+            board[randomOpenSpace()] = "O";
             setTimeout(() => {
                 showBoard();
             }, 450);
-            document.getElementById("gameInfo").value = "user turn";
+            userTurn = true;
         }
     }
     checkGameOver();
 }
 
-// makes a random move, and the player is restricted from moving during this time
-function computerMove() {
-    document.getElementById("gameInfo").value = "cpu turn";
-    board[randomOpenSpace()] = "O";
+// returns true if a given space on the board is open
+function isOpenSpace(index) {
+    return (board[index] == "");
 }
 
 // finds a random open space for the computer to make its move
 function randomOpenSpace() {
     let openSpaces = [];
-
     for(let i = 0; i < 9; i++) {
         if(board[i] == "") {
             openSpaces.push(i);
@@ -87,27 +88,25 @@ function randomOpenSpace() {
     if a win or loss is detected, the winning/losing pattern will be highlighted on the board.
 */
 function checkGameOver() {
-    for(let i = 0; i < wins.length-1; i++) {
-        if(board[wins[i][0]].concat(board[wins[i][1]]).concat(board[wins[i][2]]) == "XXX") {
+    for(let i = 0; i < wins.length; i++) {
+        if(board[wins[i][0]] + board[wins[i][1]] + board[wins[i][2]] == "XXX") {
             document.getElementById(boardElements[wins[i][0]]).style.backgroundColor= "#ADFF2F"; // green-yellow
             document.getElementById(boardElements[wins[i][1]]).style.backgroundColor= "#ADFF2F";
             document.getElementById(boardElements[wins[i][2]]).style.backgroundColor= "#ADFF2F";
 
             document.getElementById("gameInfo").innerHTML = "Victory";
-
             return true;     
         }
-        else if(board[wins[i][0]].concat(board[wins[i][1]]).concat(board[wins[i][2]]) == "OOO") {
+        else if(board[wins[i][0]] + board[wins[i][1]] + board[wins[i][2]] == "OOO") {
             document.getElementById(boardElements[wins[i][0]]).style.backgroundColor= "#FF6347"; // tomato red
             document.getElementById(boardElements[wins[i][1]]).style.backgroundColor= "#FF6347";
             document.getElementById(boardElements[wins[i][2]]).style.backgroundColor= "#FF6347";
 
             document.getElementById("gameInfo").innerHTML = "Defeat";
-
             return true;
         }
     }
-    for(space in board) {
+    for(space in board) {   
         if(board[space] == "") {
             document.getElementById("gameInfo").innerHTML = "...";
             return false;
